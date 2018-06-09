d8c72b35985d714711759fa54cce720fd6c4e7c3
Sasi Krishna <sk@recruitfox.com>
2018-06-09 20:57:03 +0530

    adding dynamic programming algorithms

---
+++ src/com/algorithms/dp/KnapsackProblem.java
@@ -0,0 +1,87 @@
+package com.algorithms.dp;
+
+/**
+ * Problem statement: Given a knapsack with some maximum capacity find maximum value can be achieved by putting weights into the sack. Each weight will have some value.
+ * <br>Constraint: This is 0 or 1 knapsack problem so we'll put whole weight into knapsack or not
+ * <br><br>
+ * Test case:
+ * <br>
+ * Input: weights = {3, 2, 1, 5} values = {6, 5, 1, 10} and sack capacity = 7
+ * <br>
+ * Output: 5 (10), 2 (5)
+ * <br>
+ * @author Sasi on 08-Jun-2018, 8:14:13 pm
+ */
+public class KnapsackProblem {
+	
+	int weights[], values[], capacity;
+	int knapsackTable[][];
+	
+	public KnapsackProblem(int[] weights, int[] values, int capacity) {
+		super();
+		this.weights = weights;
+		this.values = values;
+		this.capacity = capacity;
+		knapsackTable = new int[this.weights.length][capacity + 1];
+	}
+
+	public void findMaxWeight(){
+		for(int i = 0; i < this.weights.length; i++){
+			for(int j = 0; j <= capacity; j++){
+				
+				if(j == 0){
+					knapsackTable[i][0] = 0;							//Filling first column with considering sack capacity is zero so max can achieve is 0
+					continue;
+				}
+				
+				if(i == 0){
+					if(this.weights[i] <= j){
+						knapsackTable[i][j] = this.values[i];
+					}else{
+						knapsackTable[i][j] = 0;
+					}
+					continue;
+				}
+				
+				if(this.weights[i] > j){								//Checking if current weight is greater than sack limit if so taking max value from previous row i.e 
+					knapsackTable[i][j] = knapsackTable[i - 1][j];      //max value can be achieved without current weight.
+					continue;
+				}
+				
+				//Max(sum so far, sum so far + current weight)
+				knapsackTable[i][j] = Math.max(knapsackTable[i - 1][j], this.values[i] + knapsackTable[i - 1][j - this.weights[i]]);
+			}
+		}
+		printWeights();
+	}
+	
+	private void printWeights(){
+		int i = this.weights.length - 1, j = capacity, maxWeight = 0;
+		
+		while(i >= 0 && j >= 0){
+			
+			if(i == 0){
+				if(knapsackTable[i][j] != 0){
+					System.out.println(this.weights[0] + " (" + this.values[0] + ")");
+					maxWeight += this.values[0];
+				}
+				break;
+			}
+			
+			if(knapsackTable[i][j] != knapsackTable[i - 1][j]){			//Checking if current is equals to previous row sum. If they aren't equal then current weight is part of selection.	
+				System.out.println(this.weights[i] + " (" + this.values[i] + ")");
+				maxWeight += this.values[i];
+				j = j - this.weights[i];
+				i--;
+			}else{
+				i--;												   //Current total sum is equals to previous row sum that means it came from previous row. So current weight is not part of selection.
+			}
+		}
+		System.out.println("Max value can be put is: " + maxWeight);
+	}
+	
+	public static void main(String[] args) {
+		KnapsackProblem sack = new KnapsackProblem(new int[]{3, 2, 1, 5}, new int[]{6, 5, 1, 10}, 7);
+		sack.findMaxWeight();
+	}
+}
---
+++ src/com/algorithms/dp/ShortestCommonSuperSequence.java
@@ -0,0 +1,102 @@
+package com.algorithms.dp;
+
+/**
+ * Problem statement: Find the shortest string that has both string1 and string2 as subsequences. 
+ * <br><br>
+ * Test case:
+ * <br>
+ * Input: string1 = "AGGTAB", string2 = "GXTXAYB"
+ * <br>
+ * Output: AGXGTXAYB
+ * <br>
+ * @author Sasi on 09-Jun-2018, 9:17:41 am
+ */
+public class ShortestCommonSuperSequence {
+	
+	static int lcs[][];
+	public static String findSequence(String string1, String string2){
+		
+		int str1Len = string1.length(), str2Len = string2.length();
+		lcs = new int[str1Len + 1][str2Len + 1];
+		String lcs = findLongestSubSequence(string1, string2, str1Len, str2Len);
+		
+		StringBuffer css = new StringBuffer();
+		int i = 0, j= 0, k = 0;
+		
+		int maxStringLength = Math.max(str1Len, str2Len);
+		
+		String longString = (maxStringLength == str1Len) ? string1 : string2;
+		String shortString = (maxStringLength == str1Len) ? string2 : string1;
+		
+		/**
+		 * Merging both the strings to get common super sequence with the help of LCS
+		 */
+		while(i < longString.length() || j < shortString.length()){
+			
+			while((longString.charAt(i) != lcs.charAt(k) || k >= lcs.length()) && i < str1Len){
+				css.append(longString.charAt(i++));
+			}
+			
+			while((shortString.charAt(j) != lcs.charAt(k) || k >= lcs.length()) && j < str2Len){
+				css.append(shortString.charAt(j++));
+			}
+			
+			if(k < lcs.length()){
+				css.append(lcs.charAt(k++));
+				i++;j++;
+			}
+		}
+		
+		return css.toString();
+	}
+	
+	/**
+	 * 
+	 * @param string1
+	 * @param string2
+	 * @param str1Len
+	 * @param str2Len
+	 * @return longest sub sequence between two strings	
+	 */
+	private static String findLongestSubSequence(String string1, String string2,
+			int str1Len, int str2Len){
+		
+		for(int i = 0; i <= str1Len; i++){
+			for(int j = 0; j <= str2Len; j++){
+				if(i == 0 || j == 0){
+					lcs[i][j] = 0;
+					continue;
+				}
+				
+				if(string1.charAt(i - 1) == string2.charAt(j - 1)){
+					lcs[i][j] = 1 + lcs[i - 1][j - 1];
+					continue;
+				}
+				
+				lcs[i][j] = Math.max(Math.max(lcs[i][j - 1], lcs[i - 1][j - 1]), lcs[i - 1][j]);
+			}
+		}
+		
+		int i = str1Len, j = str2Len;
+		StringBuffer lcsString = new StringBuffer();
+		while(i > 0 && j > 0){
+			if(lcs[i][j] == (1 + lcs[i - 1][j - 1])){
+				lcsString.append(string1.charAt(i - 1));
+				i--; j--;
+			}else{
+				if(lcs[i][j] == lcs[i - 1][j]){
+					i--;
+				}else{
+					j--;
+				}
+			}
+			
+		}
+		
+		return lcsString.reverse().toString();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(findSequence("AGGTAB", "GXTXAYB"));
+	}
+}
